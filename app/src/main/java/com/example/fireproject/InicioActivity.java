ea325ca75b675b2f9a1aa37fcaf8e3b54cd778b5
Jose_Maria <josemariazapataq2@gmail.com>
2023-10-07 21:20:49 -0400

    actualizacio hackaton v2

---
+++ app/src/main/java/com/example/fireproject/InicioActivity.java
@@ -0,0 +1,30 @@
+package com.example.fireproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class InicioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+        Button button = findViewById(R.id.button);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(InicioActivity.this, ProtocolActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
